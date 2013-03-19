42ecb32eed6bcaefe71988a88e1c177ef733be62
peter <peter@sportsdraftdaily.com>
2013-03-19 11:42:58 -0400

    change name of task

--- cycleecarto/cartoapp/models.py
+++ cycleecarto/cartoapp/models.py
@@ -94,6 +94,6 @@ def cartodb_add_note(sender, instance=None, **kwargs):
          query=sql_insert_urlencoded)
     
     # send it with celery
-    cartodb_add_note.delay(url_carto_data)
+    cartodb_add_note_task.delay(url_carto_data)
 
 post_save.connect(cartodb_add_note, sender=Note)
