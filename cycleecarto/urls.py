dcec827e4327e2a082409f01b6bdd3cf902faf28
peter <peter@sportsdraftdaily.com>
2013-04-20 16:29:26 -0400

    remove /preview/ from main url

--- cycleecarto/urls.py
+++ cycleecarto/urls.py
@@ -11,19 +11,21 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', view=views.hello_view, name='hello_page'),
-    url(r'^mobile/preview/$',TemplateView.as_view(template_name='index.html'), name='preview'),    
+    url(r'^mobile/$',TemplateView.as_view(template_name='index.html'), name='preview'),    
     url(r'^init/$', view=views.init, name='init'),
-    url(r'^accounts/register/$', register, {'backend':'registration.backends.simple.SimpleBackend',
-                                           'form_class': SignupForm,
-                                           'success_url': '/' }),
+
     url(r'^m/accounts/register/$', views.register, {'backend':'registration.backends.simple.SimpleBackend',
                                            'form_class': SignupForm,
                                            'success_url': '/' }),
-
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='cyclee.login'),
     url(r'^m/accounts/login/$', views.login, name='cyclee.m_login'),
     url(r'^m/accounts/logout/$', views.logout, name='cyclee.m_logout'),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='cyclee.login'),
+    url(r'^accounts/register/$', register, {'backend':'registration.backends.simple.SimpleBackend',
+                                           'form_class': SignupForm,
+                                           'success_url': '/' }),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cartoapp/', include('cartoapp.urls')),
 )
