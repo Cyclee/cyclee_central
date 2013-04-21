771541c2dd4f530f179c9c594a78775d6620cd4c
Matthew Willse <mwillse@gmail.com>
2013-04-21 01:42:16 -0400

    pulling out var.js, notes-map.js. linking these and note-location.js. no changes.

---
+++ cycleecarto/cyclee_client/static/js/notes-map.js
@@ -0,0 +1,123 @@
+
+
+/***********
+ * =notesmap
+ *
+ * display notes on map
+ * used by map page
+ *
+**/
+var map;
+var layers = [];
+
+
+/***********
+ * =notesmap data
+ *
+ * query notes based on users filter selection
+ *
+**/
+var LayerActions = {
+  allnotes: function(){
+    layers[0].setQuery("SELECT * FROM {{table_name}}");
+    return true;
+  },
+  bikeshops: function(){
+    layers[0].setQuery("SELECT * FROM {{table_name}} WHERE category='bike shop'");
+    return true;
+  },
+  stolen: function(){
+    layers[0].setQuery("SELECT * FROM {{table_name}} WHERE description LIKE '%stolen%'");
+    return true;
+  },
+  corides: function(){
+    layers[0].setQuery("SELECT * FROM {{table_name}} WHERE category='co-ride'");
+    return true;
+  },
+  usernotes: function(){
+    layers[0].setQuery("SELECT * FROM {{table_name}} WHERE username = '"+username+"'");
+    return true;
+  },
+  
+  // commute notes -- queries a different table not yet set up
+
+}
+
+
+
+/***********
+ * =notesmap layers
+ *
+ * change data on form select
+ *
+**/
+$('#notesmap form').change( function(){
+    
+    // grab val from form
+    var filter = $('#notesmap form').find('option:selected').val();
+    console.log('map layer:' + filter);
+
+    // send it to layer action // weird
+    LayerActions[filter]();
+    
+    // clear form focus
+    $('#notesmap form select').blur();
+});
+
+
+
+/***********
+ * =notesmap init
+ *
+ * set up map on first view
+ *
+**/
+$('body').one('click','#nav-notesmap', function(){ // delegated to body to preserve order
+    mapInit();
+});
+
+
+
+/***********
+ * =notesmap init setup
+ *
+ * create map. load default data.
+ *
+**/
+
+function mapInit() {
+    console.log('new map');
+
+      // initiate leaflet map
+      map = new L.Map('map', { 
+        center: [40.72,-73.97],
+        cartodb_logo: false,
+        zoom: 12,
+        maxZoom: 15
+      })
+
+      var basemap = 'https://dnv9my2eseobd.cloudfront.net/v3/cartodb.map-pp0tkn8d/{z}/{x}/{y}.png'; // mapbox light
+
+      L.tileLayer(basemap, {
+        attribution: 'MapBox'
+      }).addTo(map);
+
+      //var layerUrl = 'http://'+cartodb_accountname+'.cartodb.com/api/v1/viz/12114/viz.json';
+      var layerUrl = 'http://'+cartodb_accountname+'.cartodb.com/api/v1/viz/'+notes_table+'/viz.json';
+
+      var layerOptions = {
+        // tile_style: "#{{table_name}}{marker-fill: #F84F40; marker-width: 8; marker-line-color: white; marker-line-width: 2; marker-clip: false; marker-allow-overlap: true;} "
+        query: "SELECT * FROM {{table_name}} WHERE category='bike shop'",
+        interactivity: "username,description,category"
+      }
+
+      cartodb.createLayer(map, layerUrl, layerOptions)
+        .on('done', function(layer) {
+          map.addLayer(layer);
+          layers.push(layer);
+        }).on('error', function() {
+        //log the error
+        });
+
+}
+
---
+++ cycleecarto/cyclee_client/static/js/vars.js
@@ -0,0 +1,55 @@
+/******************************* 
+ * Dev & Debugging
+ *
+ * send_to_central = true = app > django > cartoDB
+ *              = false = app > cartoDB
+ *
+**/
+var send_to_central = true; // true = send to django rather than directly to cartoDB
+if( send_to_central ) { console.log('Post to Central'); }
+else { console.log('Post to CartoDB'); }
+
+
+
+/******************************* 
+ * Setup Variables
+ *
+ *
+**/
+
+var height = $(window).height();
+
+
+// setup CartoDB
+var cartodb_accountname = 'cyclee';
+var cartodb_key = "api_key=d336362b5599c0e0fbcad0f6610b06d88e537e45"; 
+var notes_table = 'notes';
+var routes_table = 'routes';
+var pending_table = 'notes_pending';
+
+// old CartoDB
+    // cartodb_accountname = 'ideapublic';
+    // cartodb_key = "api_key=7302db3d477047e379af83c1987573e043022fe4"; 
+    // notes_table = 'route_flags';
+    // routes_table = 'route_paths';
+    // pending_table = 'pending_notes';
+
+var dist_route = 1000; // distance of routes to relevant commutes
+var dist_note = 1000; // distance of notes to relevant commutes
+
+var url_cartoData = 'http://'+cartodb_accountname+'.cartodb.com/api/v2/sql/?';
+var url_cartoMap = 'https://'+cartodb_accountname+'.cartodb.com/tables/' // + ?_table+'/embed_map?';
+    // note!!
+    // sql= for embed_map
+    // q= for json & geojson
+
+
+var user_location;
+var inputfield;
+var find_map = null;
+var find_lat, find_lng;
+var user_flags;
+
+var notes_limit = 35;
+var notes_format = 'format=GeoJSON&';
+var people_limit = 25;
